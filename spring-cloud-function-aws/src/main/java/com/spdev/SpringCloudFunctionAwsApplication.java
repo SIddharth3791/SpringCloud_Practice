bc1a095352f4d941884f3297f7893960b9ed50e8
Siddharth Patel <sidpatel@Siddharths-MacBook-Pro.local>
2019-06-04 23:06:16 -0400

    initialize bean and expose two new function

--- spring-cloud-function-aws/src/main/java/com/spdev/SpringCloudFunctionAwsApplication.java
+++ spring-cloud-function-aws/src/main/java/com/spdev/SpringCloudFunctionAwsApplication.java
@@ -1,11 +1,24 @@
 package com.spdev;
 
+import java.util.function.Function;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringCloudFunctionAwsApplication {
 
+	@Bean
+	public Function<String, String> uppercase(){
+		return (String request) -> request.toUpperCase();
+	}
+	
+	@Bean
+	public Function<String, String> lowercase(){
+		return (String request) -> request.toLowerCase();
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringCloudFunctionAwsApplication.class, args);
 	}
---
+++ spring-cloud-function-aws/src/main/java/com/spdev/SpringLambdaHandler.java
@@ -0,0 +1,7 @@
+package com.spdev;
+
+import org.springframework.cloud.function.adapter.aws.SpringBootRequestHandler;
+
+public class SpringLambdaHandler extends SpringBootRequestHandler<String, String>{
+
+}
