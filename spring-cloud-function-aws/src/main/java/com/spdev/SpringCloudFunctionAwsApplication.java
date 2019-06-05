a8b9ecfa2c213b90f94c2af34ea7fd90981757ca
Siddharth Patel <sidpatel@Siddharths-MacBook-Pro.local>
2019-06-04 22:45:34 -0400

    Spring cloud stub ready with dependencies

---
+++ spring-cloud-function-aws/src/main/java/com/spdev/SpringCloudFunctionAwsApplication.java
@@ -0,0 +1,13 @@
+package com.spdev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudFunctionAwsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudFunctionAwsApplication.class, args);
+	}
+
+}
